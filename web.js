25058f5574945ca4f9bd03a733d17d6e6e97a3d5
karlCobb <malec038@umn.edu>
2013-07-10 01:33:41 +0000

    add web.js

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
 var fs = require('fs');
 var buffer = new Buffer(Buffer.byteLength('Hello world from index.html'));
-buffer = fs.readFileSync('/home/ubuntu/bitstarter/index.html', 'utf-8');
+buffer = fs.readFileSync('/bitstarter/index.html', 'utf-8');
 buffer.toString();
