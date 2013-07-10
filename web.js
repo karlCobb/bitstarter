1d7156eab4b282a2ec31b66e6e54d7621c858ace
karlCobb <malec038@umn.edu>
2013-07-10 01:40:03 +0000

    add web.js

--- web.js
+++ web.js
@@ -1,4 +1,17 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
 var fs = require('fs');
+
 var buffer = new Buffer(Buffer.byteLength('Hello world from index.html'));
-buffer = fs.readFileSync('/bitstarter/index.html', 'utf-8');
-buffer.toString();
+buffer = fs.readFileSync('index.html', 'utf-8');
+
+app.get('/', function(request, response) {
+  response.send(buffer.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
