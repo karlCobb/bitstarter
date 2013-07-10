e1709ac1271ca37116a17c664977895ad2c0e001
Karl Cobb <don.dorko@gmail.com>
2013-07-09 20:49:36 -0400

    Update web.js

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
 var buffer = new Buffer(Buffer.byteLength('Hello World from index.html'));
-fs.readFileSync('/karlCobb/bitstarter/index.html');
+fs.readFileSync('/bitstarter/index.html');
 buffer.toString();
                                            
