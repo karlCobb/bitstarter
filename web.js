65039e8ca1ea9219aed2d580141def04f350ee43
karlCobb <malec038@umn.edu>
2013-07-10 01:11:00 +0000

    Buffer correction

--- web.js
+++ web.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-var buffer = new Buffer(Buffer.byteLength('Hello World from index.html'));
-fs.readFileSync('/karlCobb/bitstarter/index.html');
-buffer.toString();
-                                           
+var buffer = new Buffer(Buffer.byteLength('Hello world from index.html'));
+buffer = fs.readFileSync('/home/ubuntu/bitstarter/index.html', 'utf-8');
+console.log(buffer);                                          
