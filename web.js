f2aece4a43b67303bb6963ad3ffabb5133618783
karlCobb <malec038@umn.edu>
2013-08-06 21:12:27 +0000

    edited index.html

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var htmlfile = "index.html";
 var fs = require('fs');
 
 var buffer = new Buffer(Buffer.byteLength('Hello world from index.html'));
@@ -11,7 +12,7 @@ app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
